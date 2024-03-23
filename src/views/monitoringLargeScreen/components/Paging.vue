59502c9de5c56ee9c1cc7d4f454bbb17aa043221
Myliu0401 <66337456+Myliu0401@users.noreply.github.com>
2024-03-24 01:22:57 +0800

    添加分页组件

--- src/views/monitoringLargeScreen/components/Paging.vue
+++ src/views/monitoringLargeScreen/components/Paging.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="paging">
-        
+        <i class="iconfont icon-xiangzuo arrow" @click="switchPages" data-type="lastPage"></i>
+        <ul class="pagesBox" @click="switchPages">
+            <li class="item iconfont" v-for="num, index in buildPageCount()" :key="num + index" :class="{
+            active: activePage == num,
+            dots: num === '...',
+            left: num === '...' && index == 1,
+            right: num === '...' && index !== 1
+        }" :data-type="num === '...' ? index == 1 ? 'left' : 'right' : num">
+                {{ num === '...' ? '' : num }}
+            </li>
+        </ul>
+        <i class="iconfont icon-arrow- arrow" @click="switchPages" data-type="nextPage"></i>
     </div>
 </template>
 
@@ -8,9 +19,107 @@
 
 <script>
 export default {
-    data(){
-        return {
+    props: {
+        total: {
+            default: 100
+        },
+        quantity: {
+            default: 10
+        },
+        currentPage: {
+            default: 1
+        },
 
+    },
+    data() {
+        return {
+            activePage: this.currentPage,
+
+        }
+    },
+
+    computed: {
+        // 总页数
+        pageCount() {
+            return Math.ceil(this.total / this.quantity);
+        },
+
+        startEndPage() {
+
+            let sta = this.activePage - 2 > 2 ? this.activePage - 2 : 2;
+            const end = sta + 4 > this.pageCount - 1 ? this.pageCount - 1 : sta + 4;
+
+            return {
+                start: this.pageCount <= 7 ? end : sta - (4 - (end - sta)),
+                end: end
+            }
+        },
+
+    },
+
+    created() {
+        window.aa = this;
+    },
+
+    methods: {
+
+        // 构建页数
+        buildPageCount() {
+            const start = this.activePage - 2 > 2 ? true : false;
+            const end = this.activePage + 2 < this.pageCount - 1 ? true : false;
+
+            const obj = this.startEndPage;
+
+            const arr = [1];
+
+            for (let i = obj.start; i <= obj.end; i++) {
+                if (this.pageCount < 7) {
+                    arr.push(i)
+                    continue;
+                };
+
+                if (i === obj.start && start || i === obj.end && end) {
+                    if (i === obj.start) {
+                        arr.push('...');
+                        arr.push(i);
+                    } else {
+                        arr.push(i);
+                        arr.push('...');
+                    };
+                } else {
+
+                    arr.push(i);
+                };
+
+
+            };
+
+            arr.push(this.pageCount);
+
+            return arr;
+
+        },
+
+        // 切换页数
+        switchPages(e) {
+            const index = e.target.dataset.type;
+            if (index === 'lastPage' && this.activePage === 1
+                || index === 'nextPage' && this.activePage === this.pageCount
+                || index == this.activePage) {
+                return;
+            };
+
+            if (index === 'lastPage') {
+                this.activePage = this.activePage - 1;
+            } else if (index === 'nextPage') {
+                this.activePage = this.activePage + 1;
+            } else if (index === 'left') {
+                this.activePage = this.activePage - 4 < 1 ? 1 : this.activePage - 4;
+            } else if (index === 'right') {
+                this.activePage = this.activePage + 4 > this.pageCount ? this.pageCount : this.activePage + 4;
+            } else {
+                this.activePage = +index;
+            }
         }
     }
 }
@@ -18,5 +127,82 @@ export default {
 
 
 <style lang="scss" scoped>
-  
+.paging {
+    display: flex;
+    align-items: center;
+
+    .arrow {
+        display: flex;
+        justify-content: center;
+        cursor: pointer;
+        width: 20px;
+        height: 20px;
+        display: inline-block;
+        color: #4fbbb9;
+        cursor: pointer;
+        transition: all 0.3s;
+
+        &:hover {
+            color: #60fffc;
+        }
+
+        &:last-child {
+            margin-left: 6px;
+        }
+    }
+
+    .icon-xiangzuo {
+        &::before {
+            font-weight: bold;
+        }
+    }
+
+
+    .pagesBox {
+        display: flex;
+        align-items: center;
+
+        .item {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #4fbbb9;
+            width: 25px;
+            height: 20px;
+            transition: all 0.3s;
+            cursor: pointer;
+
+            &:hover {
+                color: #60fffc;
+            }
+
+            &.active {
+                color: #60fffc;
+            }
+
+            &.dots {
+
+                &::before {
+                    content: '...';
+                }
+
+                &.left:hover {
+
+                    &::before {
+                        content: '\eb0a';
+                    }
+                }
+
+                &.right:hover {
+                    &::before {
+                        content: '\eb09';
+                    }
+                }
+            }
+
+        }
+    }
+
+
+}
 </style>
--- src/views/monitoringLargeScreen/pages/deviceList.vue
+++ src/views/monitoringLargeScreen/pages/deviceList.vue
@@ -2,7 +2,7 @@
 	<div class="deviceListPage">
 		<div class="lietBox">
 			<div class="lietBox_titleBox">
-				<ul class="ul">
+				<ul class="ul" ref="titleBox">
 					<li class="ul_li box"></li>
 					<li class="ul_li">设备编号</li>
 					<li class="ul_li">绑定位置</li>
@@ -12,7 +12,7 @@
 					<li class="ul_li">操作</li>
 				</ul>
 			</div>
-			<div class="lietBox_content">
+			<div class="lietBox_content" ref="lietBox_content">
 				<ul class="ul">
 					<li class="ul_li">
 						<div class="li_div"></div>
@@ -242,19 +242,55 @@
 				</ul>
 			</div>
 
-            <div class="lietBox_paging">
-                
-            </div>
+			<div class="lietBox_paging">
+				<Paging />
+
+			</div>
 		</div>
 	</div>
 </template>
 
 
 <script>
+import Paging from '../components/Paging.vue';
 export default {
+	components: {
+		Paging
+	},
 	data() {
 		return {};
 	},
+
+	mounted() {
+		this.setHeaderWidth();
+	},
+
+	methods: {
+		// 修改表头宽度
+		setHeaderWidth() {
+			const fatherHeight = this.$refs.lietBox_content.offsetHeight;
+			const firstChildHeight = this.$refs.lietBox_content.firstChild.offsetHeight;
+
+			if (fatherHeight >= firstChildHeight) {
+				this.$refs.titleBox.style.width = '100%';
+			} else {
+				this.$refs.titleBox.style.width = 'calc(100% - 7px)';
+			}
+		},
+
+		handleSizeChange() {
+
+		},
+
+		handleCurrentChange() {
+
+		},
+
+		currentPage2() {
+
+		}
+
+	}
 };
 </script>
 
@@ -276,37 +312,44 @@ export default {
 			border: 1px solid rgba(99, 235, 233, 1);
 			height: 32px;
 			background-color: rgb(14, 58, 233);
+
 			.ul {
 				width: calc(100% - 7px);
 				height: 100%;
 				display: flex;
+
 				.ul_li {
-                    
+
 					list-style: none;
-					height:  100%;
+					height: 100%;
 					border-right: 1px solid rgba(99, 235, 233, 1);
 					color: #fff;
 					display: flex;
 					align-items: center;
 					justify-content: center;
 					font-size: 12px;
+
 					&:nth-child(1) {
 						width: 32px;
 						border-right: 1px solid rgba(99, 235, 233, 1);
 					}
+
 					width: 17.8%;
-					&:nth-child(2) {
-					}
+
+					&:nth-child(2) {}
+
 					&:nth-child(3) {
 						width: 30%;
 					}
+
 					&:nth-child(4) {
 						width: 7%;
 					}
-					&:nth-child(5) {
-					}
-					&:nth-child(6) {
-					}
+
+					&:nth-child(5) {}
+
+					&:nth-child(6) {}
+
 					&:nth-child(7) {
 						width: 7%;
 						border-right: none;
@@ -323,15 +366,18 @@ export default {
 			overflow-y: auto;
 
 			&::-webkit-scrollbar {
-				width: 7px; /* 滚动条宽度 */
+				width: 7px;
+				/* 滚动条宽度 */
 				cursor: pointer;
 			}
 
 			/* 轨道上的滚动条滑块 */
 			&::-webkit-scrollbar-thumb {
-				background: rgba(99, 235, 233, 0.3); /* 滑块颜色 */
+				background: rgba(99, 235, 233, 0.3);
+				/* 滑块颜色 */
 				cursor: pointer;
 			}
+
 			.ul {
 				width: 100%;
 
@@ -354,22 +400,27 @@ export default {
 					.li_div {
 						border-right: 1px solid rgba(99, 235, 233, 1);
 						height: 100%;
+
 						&:nth-child(1) {
 							width: 32px;
 						}
+
 						width: 17.8%;
-						&:nth-child(2) {
-						}
+
+						&:nth-child(2) {}
+
 						&:nth-child(3) {
 							width: 30%;
 						}
+
 						&:nth-child(4) {
 							width: 7%;
 						}
-						&:nth-child(5) {
-						}
-						&:nth-child(6) {
-						}
+
+						&:nth-child(5) {}
+
+						&:nth-child(6) {}
+
 						&:nth-child(7) {
 							width: 7%;
 							border-right: none;
@@ -378,6 +429,14 @@ export default {
 				}
 			}
 		}
+
+		.lietBox_paging {
+			display: flex;
+			justify-content: end;
+			margin-top: 20px;
+		}
+
+
 	}
 }
 </style>
